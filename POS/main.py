e8ec7dc519a3e0d9bb2907370a6c3f6baf94e8d4
Opeoluwa Fatunmbi <101920613+Opeoluwa-Fatunmbi@users.noreply.github.com>
2023-01-08 19:29:46 +0100

    Add files via upload

---
+++ POS/main.py
@@ -0,0 +1,18 @@
+#POS Software
+'''This POS Software is built to make withdrawing more easier.
+
+First feature --- Location of the major POS centres to know the closest POS shop
+closest to you.
+
+Second feature --- This is scary actually but I want to make it in such a way that we can
+order it to your house but I don't think I'll move forward with this because of security
+reasons
+
+'''
+#POS means POINT-OF-SALE
+#This software
+
+
+class POS:
+    def __init__(self, name, ) -> None:
+        pass
